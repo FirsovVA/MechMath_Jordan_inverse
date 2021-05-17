d7a946cb2d2904ea1be64bb9de0f748a0c6213ce
FirsovVA <71100830+FirsovVA@users.noreply.github.com>
2021-05-17 17:18:48 +0300

    Add files via upload

---
+++ Inverse/algorithm.h
@@ -0,0 +1,5 @@
+
+int index_max(double* matr, int n, int k);
+void swap_matrix (double* matr,int n, int i, int j);
+void swap_index (int* index, int i, int j);
+int solve(int n, double* matr, double* reverse, int* index, double* answer);
